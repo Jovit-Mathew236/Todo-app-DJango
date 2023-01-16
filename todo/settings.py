2e671c175b03809ca9a3dbdcf220eab23b20c79b
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-01-16 23:07:21 +0530

    host test

--- todo/settings.py
+++ todo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,14 +77,18 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'TODO2',
-        'USER':'postgres',
-        'PASSWORD': 'abcd123$',
-        'HOST':'localhost'
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'TODO2',
+#         'USER':'postgres',
+#         'PASSWORD': 'abcd123$',
+#         'HOST':'localhost'
+#     }
+# }
+
+DATABASES ={
+    "default" : dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
