eda8886d59a7062b40026a0884b70f73334d2cdb
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-01-17 00:01:55 +0530

    remove host

--- todo/settings.py
+++ todo/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +26,7 @@ SECRET_KEY = 'django-insecure-*c9p-)@_%=#-7^y=c%dv-7sz&-4iakj67shtqac9oes8brz&xc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -77,18 +76,14 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'TODO2',
-#         'USER':'postgres',
-#         'PASSWORD': 'abcd123$',
-#         'HOST':'localhost'
-#     }
-# }
-
-DATABASES ={
-    "default" : dj_database_url.parse(os.environ.get("DATABASE_URL"))
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'TODO2',
+        'USER':'postgres',
+        'PASSWORD': 'abcd123$',
+        'HOST':'localhost'
+    }
 }
 
 
--- todo/urls.py
+++ todo/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path,include
 
 urlpatterns = [
@@ -23,5 +22,3 @@ urlpatterns = [
     path('', include('accounts.urls')),
     path('', include('accounts.urls')),
 ]
-
-urlpatterns += staticfiles_urlpatterns()
