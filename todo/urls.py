41bb9498008c42618de6030667e750db5f057bb6
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-01-17 12:26:29 +0530

    added db

--- todo/urls.py
+++ todo/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,3 +23,4 @@ urlpatterns = [
     path('', include('accounts.urls')),
     path('', include('accounts.urls')),
 ]
+urlpatterns += staticfiles_urlpatterns()
