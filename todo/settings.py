0372f1a2a788c0707535cfd7c7dfcd6f0bb82108
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-01-17 13:04:24 +0530

    added db change settings

--- todo/settings.py
+++ todo/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,19 +76,16 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'TODO2',
-#         'USER':'postgres',
-#         'PASSWORD': 'abcd123$',
-#         'HOST':'localhost'
-#     }
-# }
-
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'TODO2',
+        'USER':'postgres',
+        'PASSWORD': 'abcd123$',
+        'HOST':'localhost'
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
