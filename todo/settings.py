e04f0cfb14016a59b46b984165a93d4dabb290f9
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-01-21 13:43:19 +0530

    hosting things are added

--- todo/settings.py
+++ todo/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,6 +88,8 @@ DATABASES = {
     }
 }
 
+DATABASES['default'] = dj_database_url.config()
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -126,6 +130,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'todopro/templates'),
     os.path.join(BASE_DIR, 'accounts/templates'),
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
